24081f8d862ed80c0d2fded2ca355e02ca344464
yyh123 <yunhaoyu@gmail.com>
2019-05-25 00:34:44 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import 'model/Post.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Home(),
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.title,
+          ),
+          Text(
+            posts[index].author,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          SizedBox(height: 16.0),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('yyh123'),
+        elevation: 0.0,
+      ),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: _listItemBuilder,
+      ),
+    );
+  }
+}
+
+class Hello extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text(
+        'hello,yyh123!',
+        style: TextStyle(
+          fontSize: 40.0,
+          color: Colors.orange,
+          fontWeight: FontWeight.bold,
+        ),
+        textDirection: TextDirection.ltr,
+      ),
+    );
+  }
+}
